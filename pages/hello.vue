c8a7bc2bd876fe6e03387018fae8a3800ce8bdb2
Akinori Nakata <nakata@real-its.com>
2023-04-19 19:53:23 +0900

    plugins

---
+++ pages/hello.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+const { $hello } = useNuxtApp();
+</script>
+
+<template>
+  <div>
+    Page: hello
+  </div>
+  <div>
+    {{ $hello('World') }}
+  </div>
+</template>
+
+<style scoped></style>
