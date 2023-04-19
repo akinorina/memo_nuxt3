561047515128ea05fbcfea617b338c6f8d462c05
Akinori Nakata <nakata@real-its.com>
2023-04-19 19:31:22 +0900

    components & NuxtLink

---
+++ components/Navbar.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup></script>
+
+<template>
+  <nav>
+    <NuxtLink to="/">Home</NuxtLink>
+    <NuxtLink to="/about">About</NuxtLink>
+    <NuxtLink to="/coupon">Coupon</NuxtLink>
+    <NuxtLink to="https://google.com" target="_blank">Google</NuxtLink>
+  </nav>
+</template>
+
+<style scoped>
+a {
+  margin-right: 5px;
+}
+</style>
---
+++ pages/coupon.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+const show = ref(false);
+
+const handleClick = () => {
+  show.value = true;
+}
+</script>
+
+<template>
+  <div>
+    <h1>Page: coupon</h1>
+    <button @click="handleClick">Coupon Get</button>
+    <coupon v-if="show" />
+  </div>
+</template>
+
+<style scoped></style>
