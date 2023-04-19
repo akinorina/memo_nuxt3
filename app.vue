9c079df3839bb2b706919b52ad25957452b8a6f7
Akinori Nakata <nakata@real-its.com>
2023-04-19 18:52:07 +0900

    pages & layouts

--- app.vue
+++ app.vue
@@ -1,5 +1,12 @@
+<script setup>
+const admin = false;
+const layout = admin ? 'custom' : 'default';
+</script>
+
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <NuxtLayout :name="layout">
+    <NuxtPage />
+  </NuxtLayout>
 </template>
+
+<style scoped></style>
---
+++ pages/samples/sample001.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div style="background-color: #f0f0f0;">
+    <NuxtLayout name="default">
+      <template #header>ヘッダー</template>
+      Page: samples/sample001
+    </NuxtLayout>
+  </div>
+</template>
+
+<style scoped></style>
