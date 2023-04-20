cc7f09b44eedad4c9a55456a222203611437de5e
Akinori Nakata <nakata@real-its.com>
2023-04-20 13:35:29 +0900

    Dynamicルーティング

---
+++ pages/users/[id].vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+// ID.
+const router = useRoute();
+console.log('id:', router.params.id)
+</script>
+
+<template>
+  <div style="background-color: #fff;">
+    <h1>User ID. {{ $route.params.id }} ページ</h1>
+  </div>
+</template>
+
+<style scoped></style>
