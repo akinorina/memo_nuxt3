4f1e2c76139af145525fcba685cc935d68ac6202
Akinori Nakata <nakata@real-its.com>
2023-04-20 13:17:24 +0900

    Navbar Link追加

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -5,6 +5,8 @@
     <NuxtLink to="/">Home</NuxtLink>
     <NuxtLink to="/about">About</NuxtLink>
     <NuxtLink to="/coupon">Coupon</NuxtLink>
+    <NuxtLink to="/counter">Counter</NuxtLink>
+    <NuxtLink to="/hello">Hello</NuxtLink>
     <NuxtLink to="https://google.com" target="_blank">Google</NuxtLink>
   </nav>
 </template>
