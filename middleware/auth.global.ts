279e831d93b9c0333fd74e9e6ad6bdc4a726a18d
Akinori Nakata <nakata@real-its.com>
2023-04-20 14:31:43 +0900

    middleware

---
+++ middleware/auth.global.ts
@@ -0,0 +1,13 @@
+/**
+ * middleware - auth
+ */
+export default defineNuxtRouteMiddleware((to, from) => {
+  // console.log('--- middleware/defineNuxtRouterMiddleware(to, from) ---')
+  // console.log('from', from)
+  // console.log('to', to)
+
+  // if (from.fullPath === '/') {
+  //   // return navigateTo('/');
+  //   return abortNavigation();
+  // }
+})
