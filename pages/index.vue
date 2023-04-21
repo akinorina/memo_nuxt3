5c89586bd2f95afb7ec93d64790ba01444958c43
Akinori Nakata <nakata@real-its.com>
2023-04-21 09:43:06 +0900

    API hello

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,11 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const { data } = useFetch('/api/hello')
+</script>
 
 <template>
   <div>
-    Page: index
+    <h1>Main Page</h1>
+    <p>{{ data }}</p>
   </div>
 </template>
 
